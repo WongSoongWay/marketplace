623efa31451f2cf15e855352acfe056f01f5473b
Soong Way <wong@Soongs-MacBook-Pro.local>
2026-03-05 19:40:48 +0000

    Set up H2 database and made Product table in the database

--- src/main/java/com/example/marketplace3/OrderController.java
+++ src/main/java/com/example/marketplace3/OrderController.java
@@ -1,13 +1,17 @@
 package com.example.marketplace3;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
-    @GetMapping("/")
-    public String home() {
-        return "first";}
+
+
+
+    @GetMapping
+    public String viewOrderHistory() {
+        return "orderHistory";  // checkout.html
+    }
 }
-
-
